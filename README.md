918ae0b0cc30cf2b2fa95f42136275a32ec3ea51
William Griffin <w.griffin13@gmail.com>
2020-07-30 12:33:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IPO-RF
+IPO Random Forest - An implementation of scikit-learn random forest regression on pre-IPO data and monthly stock price return timeseries
---
+++ startapp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o allexport
+source ./.env
+set +o allexport
+
+source .venv/bin/activate
+
+if [ "$RUN_JUPYTER" == "true" ]; then
+	pip3 install -r requirements.txt
+	jupyter notebook --no-browser --ip 0.0.0.0 --port 8888 --allow-root
+fi
